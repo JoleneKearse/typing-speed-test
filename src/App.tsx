b23fc0579d04dcfe8f1e32b50cb1a1fca3f28d67
JoleneKearse <pharaohnutz@gmail.com>
2025-12-22 13:17:19 -0500

    scaffold project with tailwind and customizations

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <p className="text-cyan-800 text-preset-1-mobile font-bold font-sora">Hello Typing Speed Test</p>
+    </>
+  )
+}
+
+export default App

ecd9522351e08b4c10bf607971bb177f7ec942fe
JoleneKearse <pharaohnutz@gmail.com>
2025-12-23 12:21:05 -0500

    chore: style Stats for tablets

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,6 +1,6 @@
 const Stats = () => {
   return (
-    <section className="grid grid-cols-3 justify-center items-center  outline outline-amber-400 md:w-4/5">
+    <section className="grid grid-cols-3 md:flex md:justify-between outline outline-amber-400 md:w-3/4">
       <div className="flex flex-col items-center gap-2 md:flex-row">
         <p className="text-preset-3-mobile md:text-preset-3 text-neutral-400">
           WPM:
@@ -12,7 +12,7 @@ const Stats = () => {
           0
         </p>
       </div>
-      <div className="flex flex-col items-center gap-2  border-l border-r border-neutral-700 md:flex-row md:pl-600">
+      <div className="flex flex-col items-center gap-2  border-l border-r border-neutral-700 md:flex-row md:pl-800 md:pr-800">
         <p className="text-preset-3-mobile md:text-preset-3 text-neutral-400">
           Accuracy:
         </p>
